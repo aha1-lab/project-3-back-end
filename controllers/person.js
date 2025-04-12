c02f2a1410c0206f0f26a36a38df056cf672a655
Hussain Tarrada <hussaintarrada@Hussains-MacBook-Air.local>
2025-04-12 04:24:56 +0300

    edit user details

--- controllers/person.js
+++ controllers/person.js
@@ -43,45 +43,19 @@ router.get("/:userId", async(req, res)=>{
 });
 
 router.put("/edit/:userId", async(req, res)=>{
-    try {
-        req.body.user = req.user._id;
-        const findPerson = await User.findByIdAndUpdate(req.params.userId, req.body,{
-            new:true
-        });
-        if(!findPerson){
-            res.status(404);
-            throw new Error('Could not find this id');
-        }
-        findPerson._doc.user = req.user;
-        res.status(200).json(findPerson);
-    } catch (error) {
-        if(res.statusCode === 404){
-            res.json({err:error.message});
-        }else{
-            res.status(500).json({err:error.message})
-        }
-    }
+    try{
+    const foundUser = await User.findById(req.params.userId)
+
+    console.log(foundUser.username)
+    console.log(req.body.user._id)
+
+    const updatedUser = await User.findByIdAndUpdate(req.params.userId,req.body,{new:true})
+    res.json(updatedUser)
+ }
+ catch(err){
+     res.status(500).json({err:err.message})
+ }
 });
 
-router.delete("/:userId", async(req, res)=>{
-    try {
-        req.body.user = req.user._id;
-        req.body.active = false;
-        const findPerson = await User.findByIdAndUpdate(req.params.userId, req.body,{
-            new:true
-        });
-        if(!findPerson){
-            res.status(404);
-            throw new Error('Could not find this id');
-        }
-        findPerson._doc.user = req.user;
-        res.status(200).json(findPerson);
-    } catch (error) {
-        if(res.statusCode === 404){
-            res.json({err:error.message});
-        }else{
-            res.status(500).json({err:error.message})
-        }
-    }
-});
+
 module.exports = router
